c20e6fdcbd3788df7e7ef27ffb659a82690ed5de
AadeshKabra <aadeshkabra@gmail.com>
2025-05-18 17:06:34 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,174 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from dotenv import load_dotenv
+import os
+import requests
+from starlette.responses import JSONResponse
+from urllib3 import request
+
+
+def generate_token():
+    url = "https://developer.hdfcsec.com/oapi/v1/login"
+    params = {
+        "api_key": HDFC_OPENAPI_KEY
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
+
+def generate_otp(client_id, password, token_id):
+    url = "https://developer.hdfcsec.com/oapi/v1/login/validate"
+    params = {
+        "api_key": HDFC_OPENAPI_KEY,
+        "token_id": token_id
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "username": client_id,
+        "password": password
+    }
+
+    response = requests.post(url, headers=headers, params=params, json=data)
+    return response.json()
+
+
+def validate_OTP(otp, token_id):
+    # print("TokenID: ", token_id)
+    url = "https://developer.hdfcsec.com/oapi/v1/twofa/validate"
+    params = {
+        "api_key": HDFC_OPENAPI_KEY,
+        "token_id": token_id
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "answer": otp
+    }
+
+    response = requests.post(url, headers=headers, params=params, json=data)
+    # print(response.status_code)
+    return response.json()
+
+
+def get_authorisation(token_id, request_token):
+    url = "https://developer.hdfcsec.com/oapi/v1/authorise"
+    params = {
+        "api_key": HDFC_OPENAPI_KEY,
+        "token_id": token_id,
+        "consent": True,
+        "request_token": request_token
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/123.0.0.0 Safari/537.36",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
+
+def get_access_token(request_token):
+    print("Secret: ", HDFC_OPENAPI_SECRET)
+    url = "https://developer.hdfcsec.com/oapi/v1/access-token"
+    params = {
+        "api_key": HDFC_OPENAPI_KEY,  # Replace with your actual API key
+        "request_token": request_token  # Replace with your actual request token
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+    }
+    data = {
+        "apiSecret": "f910408b63c54780887579fdd2525bcf"  # Replace with your actual API secret
+    }
+
+    response = requests.post(url, params=params, headers=headers, json=data)
+
+    # response = requests.post(url, headers=headers, params=params, json=data)
+    print(response.status_code)
+    return response.json()
+
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+HDFC_OPENAPI_KEY = os.getenv("HDFC_OPENAPI_KEY")
+HDFC_OPENAPI_SECRET = os.getenv("HDFC_OPENAPI_SECRET")
+
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+
+
+@app.post("/otp")
+async def otp(payload: dict):
+    print(payload)
+    data = payload["params"]
+    client_id = data["clientid"]
+    password = data["password"]
+
+    token_id = generate_token()["tokenId"]
+    print(token_id)
+    # TOKEN_ID = token_id
+    otp = generate_otp(client_id, password, token_id)
+    print(otp)
+
+    # return JSONResponse({"response": "return"})
+    # request_token = validateOTP(otp)
+
+    if "error" in otp:
+        return JSONResponse({"error": "Invalid Credentials. Please try again"})
+    else:
+        return JSONResponse({"tokenID": token_id})
+
+
+@app.post("/login")
+async def login(payload: dict):
+    print("Login payload: ", payload)
+    otp = payload["otp"]
+    token_id = payload["tokenId"]
+
+    request_token_json = validate_OTP(otp, token_id)
+    print(request_token_json)
+    request_token = request_token_json["requestToken"]
+    print("Request Token: ", request_token)
+    print("Request token generated")
+
+    authorisation_json = get_authorisation(token_id, request_token)
+    print(authorisation_json)
+    request_token_new = authorisation_json["requestToken"]
+    print("New Request Token: ", request_token_new)
+    print("Authorisation Done")
+
+    access_token_json = get_access_token(request_token_new)
+    print(access_token_json)
+    # access_token = access_token_json["accessToken"]
+
+    return JSONResponse({"Data": "received"})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ test.py
@@ -0,0 +1,132 @@
+# # from google import genai
+# #
+# #
+# # client = genai.Client(api_key="AIzaSyD7yNhzXeVZOVdMU2_oSIsWLaLuDRNFn54")
+# #
+# # response = client.models.generate_content(
+# #     model="gemini-2.0-flash",
+# #     contents="Can you tell me what is the latest news about Tata Steel Share? Also date the news"
+# # )
+# #
+# # print(response.text)
+#
+#
+# # import logging
+# # from kiteconnect import KiteConnect
+# #
+# #
+# # logging.basicConfig(level=logging.DEBUG)
+# #
+# # kite =
+#
+# from SmartApi import SmartConnect
+# import pyotp
+# from logzero import logger
+# import json
+# import requests
+#
+#
+# # smart_api = SmartConnect(api_key=api_key)
+# import http.client
+# import mimetypes
+#
+# conn = http.client.HTTPSConnection(" apiconnect.angelone.in ")
+# payload = ''
+# headers = {
+#   'Authorization': 'Bearer AUTHORIZATION_TOKEN',
+#   'Content-Type': 'application/json',
+#   'Accept': 'application/json',
+#   'X-UserType': 'USER',
+#   'X-SourceID': 'WEB',
+#   'X-ClientLocalIP': 'CLIENT_LOCAL_IP',
+#   'X-ClientPublicIP': 'CLIENT_PUBLIC_IP',
+#   'X-MACAddress': 'MAC_ADDRESS',
+#   'X-PrivateKey': api_key
+# }
+# conn.request("GET","/rest/secure/angelbroking/portfolio/v1/getHolding", payload, headers)
+#
+# res = conn.getresponse()
+# data = res.read()
+# print(data.decode("utf-8"))
+
+
+# import pyotp
+# from SmartApi import SmartConnect
+# from logzero import logger
+#
+# api_key = "pnkeIRyP"
+# username = "S783069"
+# password = "2468"
+#
+# totp = pyotp.TOTP(totp_secret).now()
+#
+# smart_api = SmartConnect(api_key)
+#
+# token = smart_api.generateToken(username)
+# session_data = smart_api.generateSession(username, password, totp)
+# refresh_token = session_data["data"]["refreshToken"]
+#
+# smart_api.getProfile(refresh_token)
+#
+# profile = smart_api.getProfile(refresh_token)
+# holdings = smart_api.holding()
+# print(profile)
+# print(holdings)
+# print("----------------")
+# print("----------------")
+#
+# for i in holdings["data"]:
+#     print(i)
+
+# try:
+#     if session_data["status"]:
+#         auth_token = session_data["data"]["jwtToken"]
+#         feed_token = smart_api.getfeedToken()
+#     else:
+#         logger.error("Login Failed")
+#
+# except Exception as e:
+#     logger.error("An error occured")
+
+
+HDFC_api = "67024e70604543e380948578e0091f99"
+HDFC_api_secret = "f910408b63c54780887579fdd2525bcf"
+
+
+import requests
+
+url = "https://developer.hdfcsec.com/oapi/v1/login"
+params = {
+    "api_key": HDFC_api
+}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print("Status Code:", response.status_code)
+print("Response Text:", response.text)
+print(response.json()["tokenId"])
+tokenId = response.json()["tokenId"]
+# print(tokenId)
+
+
+url = "https://developer.hdfcsec.com/oapi/v1/login/validate"
+params = {
+    "api_key": HDFC_api,
+    "token_id": tokenId
+}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Content-Type": "application/json"
+}
+data = {
+    "username": "157212706",
+    "password": "aadesh19"
+}
+
+response = requests.post(url, headers=headers, params=params, json=data)
+
+print("Status Code:", response.status_code)
+print("Response:", response.json())
