d2e25da2587e1adb8250292285131fe972ab8014
AadeshKabra <aadeshkabra@gmail.com>
2025-05-24 11:48:26 +0530

    holdings implementation

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ import os
 import requests
 from starlette.responses import JSONResponse, RedirectResponse
 from kiteconnect import KiteConnect
+from urllib3 import request
+
+
+def get_holdings():
+
+    url = "https://api.kite.trade/portfolio/holdings"
+    headers = {
+        "X-Kite-Version": "3",
+        "Authorization": f"token {api_key}:{access_token}"
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    return response.json()
 
 
 app = FastAPI()
@@ -25,9 +39,9 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 HDFC_OPENAPI_KEY = os.getenv("HDFC_OPENAPI_KEY")
 HDFC_OPENAPI_SECRET = os.getenv("HDFC_OPENAPI_SECRET")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+
 api_key = os.getenv("ZERODHA_API_KEY")
 api_secret = os.getenv("ZERODHA_API_SECRET")
-
 kite = KiteConnect(api_key=api_key)
 access_token = ""
 
@@ -56,6 +70,21 @@ async def kite_callback(request: Request):
     # return JSONResponse({"accessToken": access_token})
     redirect_url = f"{FRONTEND_URL}/success?access_token={access_token}"
     return RedirectResponse(url=redirect_url)
+
+
+@app.get("/holdings")
+async def holdings():
+    holdings = get_holdings()
+    print(holdings)
+    return JSONResponse({"holdings": holdings})
+
+
+# @app.get("/stock_news")
+# async def stock_news(stock: str):
+#     print(stock)
+#
+#     news = get_stock_news(stock)
+#     pass
 
 
 if __name__ == "__main__":
