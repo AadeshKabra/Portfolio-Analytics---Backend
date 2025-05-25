a252f9d784d1d51c42e907393057fd60d2a2af13
AadeshKabra <aadeshkabra@gmail.com>
2025-05-25 18:36:08 +0530

    Add News Section

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ import requests
 from starlette.responses import JSONResponse, RedirectResponse
 from kiteconnect import KiteConnect
 from urllib3 import request
+from serpapi import GoogleSearch
+from datetime import datetime
 
 
 def get_holdings():
-
     url = "https://api.kite.trade/portfolio/holdings"
     headers = {
         "X-Kite-Version": "3",
@@ -18,8 +19,26 @@ def get_holdings():
     }
 
     response = requests.get(url, headers=headers)
-    print(response.status_code)
+    # print(response.status_code)
     return response.json()
+
+
+def get_stock_news(stock):
+    params = {
+        "engine": "google_news",
+        "q": stock + " Stock",
+        "api_key": GOOGLE_SERPAPI_KEY,
+        "hl": "en",
+        "gl": "in"
+    }
+    search = GoogleSearch(params)
+    results =search.get_dict()
+
+    date_format = "%m/%d/%Y, %I:%M %p, %z UTC"
+    news = results["news_results"]
+    news_filtered = [i for i in news if "date" in i]
+    sorted_news = sorted(news_filtered, key=lambda x: datetime.strptime(x["date"], date_format), reverse=True)
+    return sorted_news
 
 
 app = FastAPI()
@@ -39,6 +58,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 HDFC_OPENAPI_KEY = os.getenv("HDFC_OPENAPI_KEY")
 HDFC_OPENAPI_SECRET = os.getenv("HDFC_OPENAPI_SECRET")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+GOOGLE_SERPAPI_KEY = os.getenv("GOOGLE_SERPAPI_KEY")
 
 api_key = os.getenv("ZERODHA_API_KEY")
 api_secret = os.getenv("ZERODHA_API_SECRET")
@@ -66,8 +86,6 @@ async def kite_callback(request: Request):
     kite.set_access_token(data["access_token"])
     access_token = data["access_token"]
 
-    # print(data["access_token"])
-    # return JSONResponse({"accessToken": access_token})
     redirect_url = f"{FRONTEND_URL}/success?access_token={access_token}"
     return RedirectResponse(url=redirect_url)
 
@@ -75,16 +93,15 @@ async def kite_callback(request: Request):
 @app.get("/holdings")
 async def holdings():
     holdings = get_holdings()
-    print(holdings)
+    # print(holdings)
     return JSONResponse({"holdings": holdings})
 
 
-# @app.get("/stock_news")
-# async def stock_news(stock: str):
-#     print(stock)
-#
-#     news = get_stock_news(stock)
-#     pass
+@app.get("/stock_news")
+async def stock_news(stock: str):
+    print("Reached stock news")
+    news = get_stock_news(stock)
+    return JSONResponse({"News": news})
 
 
 if __name__ == "__main__":
